f01cfb7bd6af4ed0de1b0f10564f2297f5823102
Chris Nunez <cenunez47@gmail.com>
2017-09-19 16:32:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(array)
+  waiting = []
+  if array.length > 0
+    array.each_with_index do |x, y|
+    waiting << "#{y+1}. #{x}"
+  end
+    x = waiting.join(" ")
+    puts "The line is currently: #{x}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array<< name
+  x = (array.rindex(name) + 1)
+  puts "Welcome, #{name}. You are number #{x} in line."
+end
+
+def now_serving(array)
+  if array.length < 1
+    puts "The line is currently empty."
+  end
+end
